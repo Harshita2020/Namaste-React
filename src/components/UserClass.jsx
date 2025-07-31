6337cddaf81ec6a6d1d14e525d780f8cd55cfc1f
Harshita2020 <harshitaautocad@gmail.com>
2025-07-31 23:25:41 +0530

    first class component created

---
+++ src/components/UserClass.jsx
@@ -0,0 +1,13 @@
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Name: Harshita</h1>
+        <h3>Location: Delhi</h3>
+        <h4>COntact: harshitaautocad@gmail.com</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass
