0e04816bcb09a1e08b8f758273ae1367bee7b8e2
Harshita2020 <harshitaautocad@gmail.com>
2025-07-31 22:52:56 +0530

    class based components

---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+const User = () => {
+    return (
+        <div>
+            <h2> Name: Harshita</h2>
+            <h3>Location: Delhi</h3>
+            <h4>COntact: harshitaautocad@gmail.com</h4>
+        </div>
+    )
+}
+
+export default User
