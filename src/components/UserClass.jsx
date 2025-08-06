3135186bb3ca0209043bf59eb271dd6764a6b338
Harshita2020 <harshitaautocad@gmail.com>
2025-08-06 23:48:55 +0530

    component lifecycle

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -2,26 +2,22 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(prop){
     super(prop);
-    console.log("PROP",prop)
 
     this.state = {
       count: 0,
-      count2:1
     }
+        console.log(this.props.name+` Constructor!`)
+
+  }
+
+  componentDidMount(){
+    console.log(this.props.name+` Component Did Mount!`)
   }
   render() {
     const {name, location, contact} = this.props
-    const handleClick = () => {
-      this.setState({
-        count: this.state.count + 1 ,
-        count2: this.state.count + 2,
-      })
-    }
+    console.log(this.props.name+" Render")
     return (
       <div sx={{"padding":"50px"}}>
-        <h1>Count: {this.state.count} </h1>
-        <h1>Count2: {this.state.count2} </h1>
-        <button onClick={handleClick}> Click Me!</button>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>COntact: {contact} </h4>
