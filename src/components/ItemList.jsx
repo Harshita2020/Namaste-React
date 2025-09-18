794eac704719a0e0c467f0dd9454495c68bcd346
Harshita2020 <harshitaautocad@gmail.com>
2025-09-19 00:12:32 +0530

    Accordion completed!

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { CDN_URL } from "../utils/constants";
 
 const ItemList = ({ data }) => {
-  // console.log("Item data, ", data);
+  console.log("Item data, ", data);
   return (
     <div>
       {data.map((item, i) => {
@@ -11,26 +11,27 @@ const ItemList = ({ data }) => {
             key={item?.card?.info?.id}
             className="m-4 p-2 border-b-2 border-gray-400 text-left flex"
           >
-            <div className="w-9/12">
+            <div className="w-9/12 h-auto">
               <div className="py-2 font-medium">
                 <div>
                   <span>{item?.card?.info?.name}</span> - &nbsp;
-                  <span>₹ {item?.card?.info?.price / 100} </span>
+                  <span>₹ {item?.card?.info?.price/100 ||  item?.card?.info?.defaultPrice/ 100} </span>
                 </div>
               </div>
               <p>{item?.card?.info?.description}</p>
             </div>
 
             <div className="w-3/12 items-center relative">
-              <div className="absolute top-4 left-4 ">
-                <button className=" bg-black text-white shadow-lg p-3 rounded-lg cursor-pointer">
-                  Add +
-                </button>
+              <div className="absolute bottom-0 right-7 ">
+                {item?.card?.info?.imageId && <button className="font-bold bg-gray-50 border border-gray-300 text-green-600 shadow-lg py-2 px-4 rounded-lg cursor-pointer">
+                  ADD
+                </button>}
               </div>
-              <img
-                src={CDN_URL + item?.card?.info?.imageId}
+              {item?.card?.info?.imageId && <img
+                src={(CDN_URL + item?.card?.info?.imageId)} 
                 className="rounded-lg m-4"
-              />
+                alt={item?.card?.info?.name}
+              />}
             </div>
           </div>
         );
--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -1,26 +1,25 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({ data }) => {
-  // console.log("DATA: ", data)
-  const [showItems, setShowItems] = useState(false)
+const RestaurantCategory = ({ data, showIndex, setShowIndex }) => {
   const handleClick = () => {
-    console.log("Clicked!!")
-    setShowItems(!showItems)
-  }
+    console.log("Clicked!!");
+    setShowIndex();
+  };
   return (
     <>
       <div className="  w-full bg-gray-100 my-4 shadow-lg p-4 rounded-2xl ">
-        <div className=" font-bold flex justify-between cursor-pointer" onClick={handleClick}>
-
-        <span>
-          {data?.title} ({data?.itemCards.length})
-        </span>
-        <span>🔽</span>
+        <div
+          className=" font-bold flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
+          <span>
+            {data?.title} ({data?.itemCards.length})
+          </span>
+          <span>🔽</span>
         </div>
-        {showItems && <ItemList data={data?.itemCards} />}
+        {showIndex && <ItemList data={data?.itemCards} />}
       </div>
-      {/* // Accordian Body */}
     </>
   );
 };
